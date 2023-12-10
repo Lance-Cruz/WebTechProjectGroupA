d2f794de25db44bb69c5e721e71cb7b0e115547a
Bartlomiej Drapinski <g00428194@atu.ie>
2023-12-10 16:05:33 +0000

    Working on the project in my branch

--- Socks.js
+++ Socks.js
@@ -1,15 +1,4 @@
-let currentImageIndex = 0;
-const images = document.querySelectorAll('.product-image');
-
-function changeImage(step) {
-    currentImageIndex += step;
-
-    if (currentImageIndex < 0) {
-        currentImageIndex = images.length - 1;
-    } else if (currentImageIndex >= images.length) {
-        currentImageIndex = 0;
-    }
-
-    const transformValue = -currentImageIndex * 100;
-    document.querySelector('.product-image-container').style.transform = `translateX(${transformValue}%)`;
+function cartCall() {
+    alert("Products successfully added to the cart");
 }
+
