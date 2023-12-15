5be9ce14a0f6f3dcea90aa76827cdef511b4baad
Bartlomiej Drapinski <g00428194@atu.ie>
2023-12-15 10:08:09 +0000

    fix js

--- Socks.js
+++ Socks.js
@@ -1,12 +1,3 @@
 function cartCall() {
     alert("Products successfully added to the cart");
-<<<<<<< HEAD
-<<<<<<< HEAD
 }
-=======
-}
-
->>>>>>> bartek
-=======
-}
->>>>>>> bcca4680422c0d7fc31b78d2ca5117f387eab55d
